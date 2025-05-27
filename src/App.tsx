01443829b92974f78a9aac3445504a4a1cb95280
MrAbbas <14amirabas@gmail.com>
2025-05-27 21:31:20 +0330

    install tailwind css and test it, in addition create some simple component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Home} from './pages'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="w-full min-h-screen flex justify-center items-start ">
+      <Home/>
     </div>
   );
 }
